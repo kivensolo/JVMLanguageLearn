75691f8c86f8e596dfcbdc5f980d7db6f7ca4e20
kivensolo <760553478@qq.com>
2016-09-19 19:11:10 +0800

    添加注解

---
+++ JavaTest/src/com/annotation/FruitColor.java
@@ -0,0 +1,19 @@
+package com.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Copyright(C) 2016, 北京视达科科技有限公司
+ * All rights reserved. <br>
+ * author: King.Z <br>
+ * date:  2016/9/19 18:22 <br>
+ * description: XXXXXXX <br>
+ */
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface FruitColor {
+    public enum Color{BLUE,RED,GREEN};
+}
--- JavaTest/src/com/reflect/DumpMethods.java
+++ JavaTest/src/com/reflect/DumpMethods.java
@@ -11,6 +11,10 @@ import java.lang.reflect.Method;
  */
 public class DumpMethods {
     public static void main(String[] args) {
+        //testmethods();
+    }
+
+    public static void testmethods() {
         try {
             String name = DumpMethods.class.getSimpleName();
             Class c = Class.forName("java.lang.String");
@@ -21,9 +25,5 @@ public class DumpMethods {
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
         }
-    }
-
-    public void testmethods(){
-
     }
 }
