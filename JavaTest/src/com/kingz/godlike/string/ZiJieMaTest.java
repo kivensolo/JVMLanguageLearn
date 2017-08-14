0ea30abfbb2fe926a49e9d1e2fdef9a106455dd2
kivensolo <760553478@qq.com>
2017-08-14 23:05:43 +0800

    添加executor执行runnable和callable

---
+++ JavaTest/src/com/kingz/godlike/string/ZiJieMaTest.java
@@ -0,0 +1,14 @@
+package com.kingz.godlike.string;
+
+/**
+ * Copyright(C) 2016, 北京视达科科技有限公司
+ * All rights reserved. <br>
+ * author: King.Z <br>
+ * date:  2017/7/26 21:54 <br>
+ * description: XXXXXXX <br>
+ */
+public class ZiJieMaTest {
+    public static void main(String[] args) {
+        int i = 127;
+    }
+}
