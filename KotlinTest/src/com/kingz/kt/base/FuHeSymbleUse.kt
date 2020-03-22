bfbcc09266b39e7132fccd47a6c529d43e6b1e52
kivensolo-homePC <kiven6969@gmail.com>
2020-03-22 21:26:33 +0800

    title(change): update kotlin code

--- KotlinTest/src/com/kingz/kt/base/FuHeSymbleUse.kt
+++ KotlinTest/src/com/kingz/kt/base/FuHeSymbleUse.kt
@@ -3,7 +3,15 @@ package com.kingz.kt.base
 /**
  * author: King.Z <br>
  * date:  2020/3/5 14:36 <br>
- * description: 复合符号?:的使用方式  <br>
+ * description: 复合符号的使用方式  <br>
+ *     If not null 缩写 ---> ?.
+ *     If not null and else 缩写  ---> ?:
+ *     if not null  执⾏代码
+ *          val value = ……
+ *          value?.let {
+ *               …… // 代码会执⾏到此处, 假如value不为null
+ *          }
+ *
  */
 object FuHeSymbleUse {
     private val varString:String? = null
---
+++ KotlinTest/src/com/kingz/kt/base/OtherClass.kt
@@ -0,0 +1,8 @@
+package com.kingz.kt.base
+
+/**
+
+ * @author: King.Z
+ * @since: 2020/3/22 20:08 <br>
+ * @desc: 展示各种class的demo
+ */
