50df444a28a918515af87b9c85f9d3ab8ab13b6d
kivensolo-work <kivensolo@gmail.com>
2021-01-04 15:12:59 +0800

    增加注释

--- JavaTest/src/com/annotation/exoDemo/SimpleExoPlayer.java
+++ JavaTest/src/com/annotation/exoDemo/SimpleExoPlayer.java
@@ -9,6 +9,8 @@ public class SimpleExoPlayer implements Player {
 
     @Override
     public @RepeatMode int getRepeatMode() {
+        //这种用法只有在android support-annotations下的时候，IDE才会报错
+        setRepeatMode(6);
         return 0;
     }
 }
