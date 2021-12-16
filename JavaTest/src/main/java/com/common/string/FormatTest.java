61995b4101cc74a12aaa9dce2db5d56765227494
kivensolo-work <kivensolo@gmail.com>
2021-12-16 19:07:00 +0800

    Title(feat): 更新文件IO学习，使用ZipOutputStream等。 Cause：N/A Solution：N/A Effect: N/A Reviewer: N/A 附加说明：N/A

--- JavaTest/src/main/java/com/common/string/FormatTest.java
+++ JavaTest/src/main/java/com/common/string/FormatTest.java
@@ -4,8 +4,6 @@ import java.util.ArrayList;
 import java.util.Formatter;
 
 /**
- * Copyright(C) 2015, 北京视达科科技有限公司
- * All rights reserved.
  * author: King.Z
  * date:  2016/1/14 10:40
  * description:
