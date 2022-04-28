680fab90003db2915a21f10148162a41a0320143
zekeWong <kivensolo@gmail.com>
2022-04-28 11:30:17 +0800

    Title(style):StringFormat格式化修改 Cause：N/A Solution：N/A Effect: N/A Reviewer: N/A 附加说明：N/A

--- JavaTest/src/main/java/com/common/string/JavaStringFormat.java
+++ JavaTest/src/main/java/com/common/string/JavaStringFormat.java
@@ -25,7 +25,7 @@ public class JavaStringFormat {
         //+使用 对于负数无效
         System.out.printf("格式参数+:显示正负数的符号：%+d与%d  和%+d %n", 99,-99,-99);
         //-使用
-        System.out.printf("格式参数-:左对齐：%-5d  %n",15);
+        System.out.printf("格式参数-:左对齐:     %-5d  %n",15);
         //补O使用
         System.out.printf("格式参数0:数字前面补0,最牛的编号是：%03d  %n", 7);
         //(使用
@@ -34,7 +34,7 @@ public class JavaStringFormat {
         System.out.printf("无Tab键的效果是：%08d  %n", 7);
         System.out.printf("字符串 的效果是：%8s  %n", "7");
         System.out.printf("有Tab键的效果是：%7d   %n", 7);
-        //.使用
+        //,使用
         System.out.printf("整数分组的效果是：%,d   %n", 9989997);
         //空格和小数点后面个数
         System.out.printf("一本书的价格是：% 50.5f元 %n", 49.8);
@@ -52,15 +52,15 @@ public class JavaStringFormat {
         System.out.println(str);
         System.out.printf("字母a的大写是：%c %n", 'A');
         System.out.printf("3>7的结果是：%b %n", 3>7);
-        System.out.printf("100的一半是：%d %n", 100/2);
+        System.out.printf("100的整数是：%d %n", 100);
         System.out.printf("100的16进制数是：%x %n", 100);
         System.out.printf("100的8进制数是：%o %n", 100);
         System.out.printf("50元的书打8.5折扣是：%f 元%n", 50*0.85);
-        System.out.printf("上面价格的16进制数是：%a %n", 50*0.85);
-        System.out.printf("上面价格的指数表示：%e %n", 50*0.85);
-        System.out.printf("上面价格的指数和浮点数结果的长度较短的是：%g %n", 50*0.85);
-        System.out.printf("上面的折扣是%d%% %n", 85);
+        System.out.printf("\t上面价格的16进制数是：%a %n", 50*0.85);
+        System.out.printf("\t上面价格的指数表示：%e %n", 50*0.85);
+        System.out.printf("\t上面价格的指数和浮点数结果的长度较短的是：%g %n", 50*0.85);
+        System.out.printf("\t上面的折扣是%d%% %n", 85);
         System.out.printf("字母A的散列码是：%h %n", 'A');
-        System.out.println("");
+        System.out.println();
     }
 }
