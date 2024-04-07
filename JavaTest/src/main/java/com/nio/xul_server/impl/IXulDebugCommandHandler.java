8cd617e5c498ff646014e0e073f7bb4821848756
zekeWong <kivensolo@gmail.com>
2024-04-07 16:02:33 +0800

    Title(feat):Add XulHttpServer code. Cause：N/A Solution：N/A Effect: N/A Reviewer: N/A 附加说明：N/A

---
+++ JavaTest/src/main/java/com/nio/xul_server/impl/IXulDebugCommandHandler.java
@@ -0,0 +1,11 @@
+package com.nio.xul_server.impl;
+
+
+import com.nio.xul_server.XulHttpServer;
+
+/**
+ * Created by hy on 2015/12/3.
+ */
+public interface IXulDebugCommandHandler {
+	XulHttpServer.XulHttpServerResponse execCommand(String url, XulHttpServer.XulHttpServerHandler serverHandler, XulHttpServer.XulHttpServerRequest request);
+}
