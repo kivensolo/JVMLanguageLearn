0e1ef159411e1a1b762771249f34c01b3343a381
zekeWong <kivensolo@gmail.com>
2024-06-13 17:20:55 +0800

    Title(learn): kotlin协程复习，复习协程的async构造器的使用。 Cause：N/A Solution：N/A Effect: N/A Reviewer: N/A 附加说明：N/A

--- KotlinTest/src/main/kotlin/com/kingz/kt/coroutines/guide/composingSuspendingFunctions/AsyncFunction.kt
+++ KotlinTest/src/main/kotlin/com/kingz/kt/coroutines/guide/composingSuspendingFunctions/AsyncFunction.kt
@@ -1,9 +1,6 @@
 package com.kingz.kt.coroutines.guide.composingSuspendingFunctions
 
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.CoroutineStart
-import kotlinx.coroutines.async
-import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.*
 import kotlin.system.measureTimeMillis
 
 /**
@@ -14,19 +11,23 @@ import kotlin.system.measureTimeMillis
 
 fun main() = runBlocking<Unit> {
     invokeFunctionWithAsync()
+
+    invokeFunctionWithLazyAsync()
 }
 
 /**
  *  使用 async 并发
  *  注意: 使用协程进行并发总是显式的
  */
-private suspend fun CoroutineScope.invokeFunctionWithAsync() {
-    val time = measureTimeMillis {
-        val one = async { doSomethingUsefulOne() }
-        val two = async { doSomethingUsefulTwo() }
-        println("The answer is ${one.await() + two.await()}")
+private suspend fun invokeFunctionWithAsync() {
+    coroutineScope {
+        val time = measureTimeMillis {
+            val one = async { doSomethingUsefulOne() }
+            val two = async { doSomethingUsefulTwo() }
+            println("The answer is ${one.await() + two.await()}")
+        }
+        println("Completed in $time ms")
     }
-    println("Completed in $time ms")
 }
 
 /**
@@ -34,17 +35,24 @@ private suspend fun CoroutineScope.invokeFunctionWithAsync() {
  * async 可以通过将 start 参数设置为 CoroutineStart.LAZY 而变为惰性的。
  * 在这个模式下，只有结果通过 await 获取的时候协程才会启动，或者在 Job 的 start 函数调用的时候。
  */
-private suspend fun CoroutineScope.invokeFunctionWithLazyAsync() {
-    val time = measureTimeMillis {
-        val one = async(start = CoroutineStart.LAZY) { doSomethingUsefulOne() }
-        val two = async(start = CoroutineStart.LAZY) { doSomethingUsefulTwo() }
-        // 定义后，协程并没有立即执行
+private suspend fun invokeFunctionWithLazyAsync() {
+    coroutineScope {
+        val time = measureTimeMillis {
+            val one = async(start = CoroutineStart.LAZY) {
+                doSomethingUsefulOne()
+            }
+            val two = async(start = CoroutineStart.LAZY) {
+                doSomethingUsefulTwo()
+            }
+            // 定义后，协程并没有立即执行
+            delay(2000L)
+            // doSomething..... 执行一些计算
 
-        // doSomething..... 执行一些计算
-
-        one.start() // 启动第一个
-        two.start() // 启动第二个
-        println("The answer is ${one.await() + two.await()}")
+            println("Start async coroutine....")
+            one.start() // 启动第一个
+            two.start() // 启动第二个
+            println("The answer is ${one.await() + two.await()}")
+        }
+        println("Completed in $time ms")
     }
-    println("Completed in $time ms")
 }
